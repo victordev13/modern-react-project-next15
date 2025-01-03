143475eb0486e855df5938dff88a019e5d00833d
victordev13 <vtrcarvalho.13@gmail.com>
2025-01-02 21:44:46 -0300

    feat: setup dashboard with shadcn ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,29 @@
-export default function Login() {
-  return <div>welcome</div>
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from '@/app/components/ui/card'
+import ToggleTheme from '../components/toggle-theme'
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Card className="container mx-auto max-w-screen-md">
+        <CardHeader>
+          <CardTitle>Dashboard</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Welcome to your dashboard!</p>
+          <ToggleTheme />
+        </CardContent>
+        <CardFooter>
+          <button>Logout</button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from './components/theme-provider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   )
